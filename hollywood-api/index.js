3300d475e57aa269306c395f7449225f8a97510c
Sarah Nicolls <sarah.goossen@hotmail.com>
2017-11-01 10:25:09 -0700

    added route boilerplate

---
+++ hollywood-api/index.js
@@ -0,0 +1,25 @@
+const hapi = require("hapi");
+
+const server = new hapi.Server();
+
+const routes = require("./routes");
+
+server.connection({
+  host: "localhost",
+  port: 4040,
+  routes: {
+    cors: true
+  },
+  router: {
+    stripTrailingSlash: true
+  }
+});
+
+server.route(routes);
+
+server.start(err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server started at ${server.info.uri}`);
+});
